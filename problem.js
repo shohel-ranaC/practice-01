13edc75087d2a015c878b20ff46bca8352c9279d
shohel-ranaC <akashshohel422@gmail.com>
2023-01-27 04:36:39 -0800

    javaScript basic problem solving

---
+++ problem.js
@@ -0,0 +1,35 @@
+// practice problem-01
+
+// console.log(900);
+
+var harryMomGive = 1000;
+var buy = 700;
+getBackMoney = harryMomGive - buy;
+console.log(getBackMoney);
+
+// Practice problem-02
+
+var mathematics = 75.25;
+var biology = 65;
+var Chemistry = 80;
+var physics = 35.45;
+var bangla = 99.5;
+
+var totalMark = mathematics + biology + Chemistry + physics + bangla;
+var averageMark = totalMark / 5;
+averageFixedValue = averageMark.toFixed(2);
+console.log(averageFixedValue);
+
+// practice problem-03
+
+var firstInput = " I am going to be";
+var secondInput = "an awesome web developer";
+
+stringMarge = firstInput + " " + secondInput;
+console.log(stringMarge);
+
+// practice problem-04
+
+var giveValue = 119;
+reminder = giveValue % 5;
+console.log(reminder);
